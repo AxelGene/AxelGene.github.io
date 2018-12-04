af85b9035e67ea49fb701a96f3bf7a1c1b22c24b
AxelGene <45603442+AxelGene@users.noreply.github.com>
2018-12-04 18:04:01 +0100

    Add files via upload

---
+++ data/Salles_1.js
@@ -0,0 +1,13 @@
+var json_Salles_1 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nom": "Salle Beauce (Maison municipale de Vienne)", "Adresse": "1 rue Dupré", "Ville": "Blois" }, "geometry": { "type": "Point", "coordinates": [ 1.343702133349647, 47.581791015984066 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Nom": "Salle du Pavillon", "Adresse": "3 rue Dupré", "Ville": "Blois" }, "geometry": { "type": "Point", "coordinates": [ 1.34395927734142, 47.582025251333732 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Nom": "Salle Jean Cros", "Adresse": "131 route de Châteaurenault", "Ville": "Blois" }, "geometry": { "type": "Point", "coordinates": [ 1.301082165072161, 47.582440349379041 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Nom": "Espace Jorge Semprun", "Adresse": "25 rue Jean-Baptiste Charcot", "Ville": "Blois" }, "geometry": { "type": "Point", "coordinates": [ 1.303482175662045, 47.59401729360566 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Nom": "Pavillon de Brisebarre", "Adresse": null, "Ville": "Blois" }, "geometry": { "type": "Point", "coordinates": [ 1.309814071732972, 47.585003506895802 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Nom": "Grande Salle de Brisebarre", "Adresse": null, "Ville": "Blois" }, "geometry": { "type": "Point", "coordinates": [ 1.310007479179776, 47.585285164903524 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Nom": "Salle Dorgelès Preau", "Adresse": "18 rue Roland Dorgelès", "Ville": "Blois" }, "geometry": { "type": "Point", "coordinates": [ 1.309704181138198, 47.581700583068688 ] } }
+]
+}
